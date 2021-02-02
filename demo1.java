27e3d8398bfcd661dbae5e75d68b09477f6d6297
Ubuntu <ubuntu@ip-172-31-28-247.us-east-2.compute.internal>
2021-02-02 01:47:27 +0000

    edited print message

--- demo1.java
+++ demo1.java
@@ -1,6 +1,6 @@
 class demo1 {
   public static void main(String args[]){
    System.out.println("Hello java from demo1");
-   System.out.println("again trying to edit");
-  }
+   System.out.println("shantu again trying to edit");
+   }
 } 
